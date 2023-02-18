34cfbb6a79f94d07998b1a12c9c0832c15c5c415
danilgryaznev <daniel58912007@gmail.com>
2023-02-18 05:29:22 +0300

    Create migrations and add relationships between database table

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Post
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post query()
+ * @mixin \Eloquent
+ */
+class Post extends Model
+{
+    use HasFactory;
+    protected $table = 'posts';
+    protected $guarded = false;
+}
