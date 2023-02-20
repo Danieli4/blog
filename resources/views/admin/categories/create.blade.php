ed87a67077f5b9428fdb81f88984737146d7ae3c
danilgryaznev <daniel58912007@gmail.com>
2023-02-20 22:17:18 +0300

    Add frontend for crate categories

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -7,7 +7,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Dashboard</h1>
+                        <h1 class="m-0">Добавление категории</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-ri ght">
@@ -25,13 +25,13 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-                    <div class="col-1">
-                        <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Создать</a>
-                    </div>
                     <div class="col-12">
-                        Категории
-                    </div>
-                </div>
+                    <form action="#" class="w-25">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Название категории">
+                        </div>
+                        <input type="submit" class="btn btn-primary" value = "Добавить">
+
                 <!-- /.row -->
 
             </div><!-- /.container-fluid -->
