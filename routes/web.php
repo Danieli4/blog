541db3394dc00b163cbb69cce4a51ed8d2074f9f
danilgryaznev <daniel58912007@gmail.com>
2023-03-07 21:53:16 +0300

    Send verification email massage Laravel create Blog 28 Laravel создание Блога 29

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::group(['namespace' =>'App\Http\Controllers\Main'], function () {
     Route::get('/', "IndexController");
 });
 
-Route::group(['namespace' =>'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth','admin']], function () {
+Route::group(['namespace' =>'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth','admin','verified']], function () {
     Route::group(['namespace' =>'Main'], function () {
         Route::get('/', "IndexController");
     });
@@ -61,6 +61,6 @@ Route::group(['namespace' =>'App\Http\Controllers\Admin', 'prefix' => 'admin', '
     });
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 
