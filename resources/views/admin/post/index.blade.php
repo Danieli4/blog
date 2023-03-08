1f5f11fb82e08650abdc6ca982e0883f01295dcf
danilgryaznev <daniel58912007@gmail.com>
2023-03-08 08:04:28 +0300

    Edite and localization interface

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -10,9 +10,9 @@
                         <h1 class="m-0">Посты</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-ri ght">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{route('admin.main.index')}}">Главная</a></li>
+                            <li class="breadcrumb-item active">Посты</a></li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
