429e86fcb6311c275eae02f632f9ec62b3b0b500
danilgryaznev <daniel58912007@gmail.com>
2023-03-08 16:41:28 +0300

    Create personal account user

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,25 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::group(['namespace' =>'App\Http\Controllers\Main'], function () {
+Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', "IndexController");
 });
-
-Route::group(['namespace' =>'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth','admin','verified']], function () {
-    Route::group(['namespace' =>'Main'], function () {
+Route::group(['namespace' => 'App\Http\Controllers\Personal', 'prefix' => 'personal', 'middleware' => ['auth', 'verified']], function () {
+    Route::group(['namespace' => 'Main', 'prefix' => 'main'], function () {
+        Route::get('/', "IndexController")->name('personal.main.index');
+    });
+    Route::group(['namespace' => 'Liked', 'prefix' => 'likeds'], function () {
+        Route::get('/', "IndexController")->name('personal.liked.index');
+    });
+    Route::group(['namespace' => 'Comment', 'prefix' => 'comments'], function () {
+        Route::get('/', "IndexController")->name('personal.comment.index');
+    });
+});
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth', 'admin', 'verified']], function () {
+    Route::group(['namespace' => 'Main'], function () {
         Route::get('/', "IndexController")->name('admin.main.index');
     });
-    Route::group(['namespace' =>'Post', 'prefix' => 'posts'], function () {
+    Route::group(['namespace' => 'Post', 'prefix' => 'posts'], function () {
         Route::get('/', "IndexController")->name('admin.post.index');
         Route::get('/create', "CreateController")->name('admin.post.create');
         Route::post('/', "StoreController")->name('admin.post.store');
@@ -29,8 +39,7 @@ Route::group(['namespace' =>'App\Http\Controllers\Admin', 'prefix' => 'admin', '
         Route::patch('/{post}', "UpdateController")->name('admin.post.update');
         Route::delete('/{post}/delete', "DeleteController")->name('admin.post.delete');
     });
-
-    Route::group(['namespace' =>'Category', 'prefix' => 'categories'], function () {
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
         Route::get('/', "IndexController")->name('admin.category.index');
         Route::get('/create', "CreateController")->name('admin.category.create');
         Route::post('/', "StoreController")->name('admin.category.store');
@@ -39,8 +48,7 @@ Route::group(['namespace' =>'App\Http\Controllers\Admin', 'prefix' => 'admin', '
         Route::patch('/{category}', "UpdateController")->name('admin.category.update');
         Route::delete('/{category}/delete', "DeleteController")->name('admin.category.delete');
     });
-
-    Route::group(['namespace' =>'Tag', 'prefix' => 'tags'], function () {
+    Route::group(['namespace' => 'Tag', 'prefix' => 'tags'], function () {
         Route::get('/', "IndexController")->name('admin.tag.index');
         Route::get('/create', "CreateController")->name('admin.tag.create');
         Route::post('/', "StoreController")->name('admin.tag.store');
@@ -49,8 +57,7 @@ Route::group(['namespace' =>'App\Http\Controllers\Admin', 'prefix' => 'admin', '
         Route::patch('/{tag}', "UpdateController")->name('admin.tag.update');
         Route::delete('/{tag}/delete', "DeleteController")->name('admin.tag.delete');
     });
-
-    Route::group(['namespace' =>'User', 'prefix' => 'users'], function () {
+    Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
         Route::get('/', "IndexController")->name('admin.user.index');
         Route::get('/create', "CreateController")->name('admin.user.create');
         Route::post('/', "StoreController")->name('admin.user.store');
@@ -60,7 +67,6 @@ Route::group(['namespace' =>'App\Http\Controllers\Admin', 'prefix' => 'admin', '
         Route::delete('/{user}/delete', "DeleteController")->name('admin.user.delete');
     });
 });
-
 Auth::routes(['verify' => true]);
 
 
